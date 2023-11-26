154b7d644d43c54e71eaefcbf1f7491c3c1082fb
PetxCode <brighterdayscodelab@gmail.com>
2023-11-26 17:57:25 +0100

    image push updated

--- dist/utils/dbConfig.js
+++ dist/utils/dbConfig.js
@@ -12,9 +12,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConfig = void 0;
 const mongoose_1 = require("mongoose");
 // const URL: string = "mongodb://127.0.0.1:27017/pickDB";
-const URL = "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.upn8ipm.mongodb.net/pickDB?retryWrites=true&w=majority";
+// const URL: string =
+//   "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.upn8ipm.mongodb.net/pickDB?retryWrites=true&w=majority";
 // const URL: string =
 //   "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.6d4tpug.mongodb.net/pickDB?retryWrites=true&w=majority";
+const URL = "mongodb+srv://shecodesaj:shecodesaj@cluster0.xe1jgnf.mongodb.net/pickDB?retryWrites=true&w=majority";
 const dbConfig = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield (0, mongoose_1.connect)(URL).then(() => {
--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -1,10 +1,12 @@
 import { connect } from "mongoose";
 
 // const URL: string = "mongodb://127.0.0.1:27017/pickDB";
-const URL: string =
-  "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.upn8ipm.mongodb.net/pickDB?retryWrites=true&w=majority";
+// const URL: string =
+//   "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.upn8ipm.mongodb.net/pickDB?retryWrites=true&w=majority";
 // const URL: string =
 //   "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.6d4tpug.mongodb.net/pickDB?retryWrites=true&w=majority";
+const URL: string =
+  "mongodb+srv://shecodesaj:shecodesaj@cluster0.xe1jgnf.mongodb.net/pickDB?retryWrites=true&w=majority";
 
 export const dbConfig = async () => {
   try {
